7f494e4540a5c59f698359b5b02466c0e75aa5be
PRANABENDU <PRANABENDU@LAPTOP-A4ROGLP3>
2023-04-21 14:43:58 +0530

    done

---
+++ MavenPractice/src/test/java/Generic/Excel.java
@@ -0,0 +1,29 @@
+package Generic;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class Excel {
+	public static String excel(int r,int c) throws EncryptedDocumentException, IOException {
+		
+		FileInputStream f1 = new FileInputStream("./excel/Book1.xlsx");
+		Workbook book = WorkbookFactory.create(f1);
+		Sheet sheet = book.getSheet("Sheet1");
+		
+		
+		Row row = sheet.getRow(r);
+		Cell cell = row.getCell(c);
+		String data = cell.toString();
+			
+		
+	
+		return data;
+	}
+}
